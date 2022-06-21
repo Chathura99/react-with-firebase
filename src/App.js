5442519c3fa6fb7e824948813ca07bda59f75abb
Chathura <chathura.madura11@gmail.com>
2022-06-21 13:21:07 +0530

    add home component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-
+import Home from './pages/Home';
 function App() {
   return (
     <div className="App">
       <h1>Hello </h1>
       <h3>Connect firebase - firestore with react app</h3>
+      <Home/>
     </div>
   );
 }
